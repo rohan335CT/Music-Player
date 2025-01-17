4c170941dde8ed587bb9444a794518be0d2998c4
Rohan K <rohan.k@CTMC9YC452.local>
2025-01-17 11:16:13 +0530

    first commit

---
+++ src/main/java/com/example/demo/Artist.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.util.*;
+
+public class Artist {
+    String artistName;
+    Set<Pair<Integer, String>> topSong;
+    HashMap<String, Integer> list;
+
+    public Artist(String artistName){
+        this.artistName = artistName;
+        this.topSong = new TreeSet<>(
+                Comparator.comparing(Pair::getKey));
+        this.list = new HashMap<>();
+    }
+
+    public void addSong(String song) {
+        if (list.containsKey(song)) {
+            list.replace(song, list.get(song) + 1);
+        } else {
+            list.put(song, 1);
+        }
+
+        Pair<Integer, String> p = new Pair<>(list.get(song) - 1, song);
+        topSong.remove(p);
+        Pair<Integer, String> p0 = new Pair<>(list.get(song) - 1, song);
+        topSong.add(p0);
+    }
+
+    public String topSong(){
+        Pair<Integer, String> biggestPair = ((TreeSet<Pair<Integer, String>>) topSong).last();
+        return biggestPair.getValue();
+    }
+
+}
---
+++ src/main/java/com/example/demo/DateClass.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class DateClass {
+    LocalDate d;
+    Set<Pair<Integer, String>> topSong;
+    HashMap<String, Integer> list;
+
+    public DateClass(LocalDate d){
+        this.d = d;
+        this.topSong = new TreeSet<>(
+                Comparator.comparing(Pair::getKey));
+        this.list = new HashMap<>();
+    }
+
+    public void addSong(String song) {
+        if (list.containsKey(song)) {
+            list.replace(song, list.get(song) + 1);
+        } else {
+            list.put(song, 1);
+        }
+
+        Pair<Integer, String> p = new Pair<>(list.get(song) - 1, song);
+        topSong.remove(p);
+        Pair<Integer, String> p0 = new Pair<>(list.get(song) - 1, song);
+        topSong.add(p0);
+    }
+
+    public String topSong(){
+        Pair<Integer, String> biggestPair = ((TreeSet<Pair<Integer, String>>) topSong).last();
+        return biggestPair.getValue();
+    }
+}
